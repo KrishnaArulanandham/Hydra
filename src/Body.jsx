abe1d71233ba070b1ca88392e5ccd8d485cc5a48
Krishna <krishnaarulanandham@gmail.com>
2024-09-08 20:27:19 +0530

    Update

---
+++ src/Body.jsx
@@ -0,0 +1,20 @@
+const Action = ({}) => {
+  return (
+    <>
+      <div className="container">
+        <div className="intakes">
+          <div className="label">
+            <p>
+              <b>TYPE OF THE WATER FOOTPRINTS</b>
+            </p>
+          </div>
+          <div className="field">
+            <input type="text" placeholder="Enter the water footprints" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Action;
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+const Header = ({}) => {
+  return (
+    <header className="container">
+      <div class="gradient-background d-flex align-items-center justify-content-center">
+        <div className="row justify-content-center mt-3 mb-4">
+          <div className="col-8">
+            <h1>WATER FOOPRINTS</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
