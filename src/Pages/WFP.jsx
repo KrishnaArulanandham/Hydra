6130e715b12b7497148bec0497913249d66d7926
Krishna <krishnaarulanandham@gmail.com>
2024-09-11 12:01:49 +0530

    update

--- src/Pages/WFP.jsx
+++ src/Pages/WFP.jsx
@@ -3,12 +3,21 @@ const WFP = ({}) => {
     <>
       <div className="text-center">
         <div className="wf1">
+          <img
+            className="col-3 d-flex justify-content-end"
+            src="wfp.jpg"
+            style={{ width: "100px" }}
+          />
           <h2>WATER FOOTPRINTS</h2>
         </div>
       </div>
-      <div>
-        <p>Types of Products :</p>
-        <input type="text" placeholder="Enter item name" />
+      <div className="input">
+        <p className="lbl1">Types of Products :</p>
+        <input
+          type="text"
+          placeholder="Enter item name"
+          style={{ width: "400px", height: "40px" }}
+        />
       </div>
     </>
   );
