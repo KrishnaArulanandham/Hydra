8f6c5a8d54662346e1e53d4762ef452e50b4b3d5
Krishna <krishnaarulanandham@gmail.com>
2024-09-14 22:12:42 +0530

    update

--- src/Pages/WFP.jsx
+++ src/Pages/WFP.jsx
@@ -31,24 +31,30 @@ const WFP = () => {
       <div className="container">
         <div className="row">
           <div className="col-10" style={{ paddingBlock: "3rem" }}>
-            <h1 class="display-6 text-center">Types of Products :</h1>
+            <h1 className="display-4 text-center">Types of Products :</h1>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="col-7">
-              <div class="input-group" style={{ width: "500px" }}>
-                <span class="input-group-text" id="inputGroup-sizing-default">
-                  Product Name
-                </span>
-                <input
-                  type="text"
-                  class="form-control"
-                  aria-label="Sizing example input"
-                  aria-describedby="inputGroup-sizing-default"
-                />
+      </div>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 col-lg-6">
+            <nav className="navbar navbar-light">
+              <div className="container-fluid">
+                <h2 className="display-6">Products</h2>
+                <form className="d-flex">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    style={{ flexGrow: 1 }}
+                  />
+                  <button className="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
               </div>
-            </div>
+            </nav>
           </div>
         </div>
       </div>
