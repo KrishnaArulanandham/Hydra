2b6877d8fda869c688097ffc97c083f36a296d25
Krishna <krishnaarulanandham@gmail.com>
2024-09-19 22:57:58 +0530

    pp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Header from "./Components/Header.js";
-import Action from "./Components/Body.js";
-import Content from "./Components/Contents.js";
-import Footer from "./Components/Footer.js";
+import Header from "./Components/Header.jsx";
+import Action from "./Components/Body.jsx";
+import Content from "./Components/Contents.jsx";
+import Footer from "./Components/Footer.jsx";
+/**
+ * App is the main component of the application.
+ *
+ * It renders the main elements of the page:
+ *   - Header: contains the navigation bar
+ *   - Action: contains the call-to-action section
+ *   - Content: contains the main content of the page
+ *   - Footer: contains the footer of the page
+ *
+ * The component uses the useState hook to keep track of the count state.
+ * The count state is not currently used, but it could be used to track the number
+ * of times the user has clicked on the call-to-action button.
+ */
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
